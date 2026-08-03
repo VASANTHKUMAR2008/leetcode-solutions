b7e7482c5c902fdbe0b3ed9c922acd5ac911f40a
VASANTHKUMAR2008 <kavithavasanth179@gmail.com>
2026-08-03 14:59:55 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (36.22%) - LeetSync

---
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] runningSum(int[] nums) {
+        int left =0;
+        int temp=0;
+        int right =0;
+        int n=nums.length;
+        int b=0;
+        for(right=1;right<n;right++){
+            temp=nums[left]+nums[right];
+             b= b+temp;
+            nums[right]=temp;
+            left++;        }
+         return nums; 
+    }
+  
+}
